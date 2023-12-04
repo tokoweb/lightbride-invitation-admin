6f50e3d7b2da295b3dd04f37dce163dd1c442b8c
samansenju <samansenju@gmail.com>
2023-12-04 11:01:40 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,17 @@ echo "Running on Branch $1"
 echo "Fetch latest code on branch $1"
 git pull origin $1
 
+echo "set up npm env"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm use 20
+
 echo "Instaling package"
-$HOME/.nvm/versions/node/v20.10.0/bin/yarn install
+yarn install
 
 echo "Build App And Restart"
-$HOME/.nvm/versions/node/v20.10.0/bin/yarn deploy:prod
+yarn deploy:prod
 
 echo "Save PID"
 pm2 save --force
