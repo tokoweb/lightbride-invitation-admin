423fac3a407add9de75d3f52714a4a066d96bd3e
samansenju <samansenju@gmail.com>
2023-12-04 10:41:21 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ echo "Fetch latest code on branch $1"
 git pull origin $1
 
 echo "Instaling package"
-yarn install
+$HOME/.nvm/versions/node/v20.10.0/bin/yarn install
 
 echo "Build App And Restart"
-yarn deploy:prod
+$HOME/.nvm/versions/node/v20.10.0/bin/yarn deploy:prod
 
 echo "Save PID"
 pm2 save --force
