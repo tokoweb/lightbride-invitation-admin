91f733e7f37154656a0f4362ce6f8433f629bc5a
Saman Supriadi <samansenju@gmail.com>
2023-12-04 02:52:14 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,10 @@ echo "Fetch latest code on branch $1"
 git pull origin $1
 
 echo "Instaling package"
-
 yarn install
 
 echo "Build App And Restart"
-
 yarn deploy:prod
 
+echo "Save PID"
 pm2 save --force
